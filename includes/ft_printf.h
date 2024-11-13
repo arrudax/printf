6b3f913f97875b694578f4ee57e4338c8068ca04
Marco Arruda <maanton2@student.42sp.org.br>
2024-11-13 08:03:21 -0300

    UPDATE: Add new prototype functions

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -3,19 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maanton2 <maanton2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: maanton2 <maanton2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 06:10:27 by maanton2          #+#    #+#             */
-/*   Updated: 2024/11/10 08:29:55 by maanton2         ###   ########.org.br   */
+/*   Updated: 2024/11/13 07:31:10 by maanton2         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <stdio.h>
+# include <stdarg.h>
+# include <stdio.h>
 
-int		ft_printf(const char *, ...);
+int		ft_printf(const char *format, ...);
+int		ft_vsprintf(char *fmt, va_list args);
+char	*ft_parse_precision_and_width(char *fmt, int *precision, int *field_width);
+char	*ft_handler_case(int c, va_list args);
+int		ft_format_string(char *formated_str, int precision, int field_width);
 
 #endif
